f04b323186ed90a46bff59a0ba9fc18adf74d809
Relintai <relintai@gmail.com>
2016-05-31 14:58:06 +0200

    .Properly incremented version.

--- data/global.lua
+++ data/global.lua
@@ -2,7 +2,7 @@
 Rekt = LibStub("AceAddon-3.0"):NewAddon("Rekt", "AceConsole-3.0", "AceEvent-3.0")
 Rekt.appName = "Rekt"
 Rekt.dbName = "RektDB"
-Rekt.version = "1.06"
+Rekt.version = "1.11"
 
 Rekt.modules = {}
 
